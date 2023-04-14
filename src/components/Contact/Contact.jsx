141563d8c4c454ae62203a8e0e7f3e6a331bf687
Guipegoraro <gui.peg@hotmail.com>
2023-04-14 15:32:17 -0300

    formulario atualizado
    formulario organizado

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,110 +1,138 @@
 import './Contact.css';
 import React, { useState } from 'react';
-function Contact () {
-    const [nome, setNome] = useState('');
-    const [idade, setIdade] = useState('');
-    const [email, setEmail] = useState('');
-    const [profissional, setProfissional] = useState('livreEscolha');
-    const [telefone, setTelefone] = useState('');
-    const [assunto, setAssunto] = useState('');
-    const [tipoDeAtendimento, setTipoDeAtendimento] = useState('');
-    const [mensagem, setMensagem] = useState('');
-    const [validated, setValidated] = useState('');
-    const [nomeValidated, setNomeValidated] = useState('');
-    const [idadeValidated, setIdadeValidated] = useState('');
-    const [emailValidated, setEmailValidated] = useState('');
-    const [telefoneValidated, setTelefoneValidated] = useState('');
-    const [tipoDeAtendimentoValidated, setTipoDeAtendimentoValidated] = useState('');
+import axios from 'axios';
+function Contact() {
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState('');
+  const [email, setEmail] = useState('');
+  const [profissional, setProfissional] = useState('livreEscolha');
+  const [telefone, setTelefone] = useState('');
+  const [assunto, setAssunto] = useState('');
+  const [tipoDeAtendimento, setTipoDeAtendimento] = useState('');
+  const [mensagem, setMensagem] = useState('');
+  const [validated, setValidated] = useState('');
+  const [nomeValidated, setNomeValidated] = useState(true);
+  const [idadeValidated, setIdadeValidated] = useState(true);
+  const [emailValidated, setEmailValidated] = useState(true);
+  const [telefoneValidated, setTelefoneValidated] = useState(true);
+  const [tipoDeAtendimentoValidated, setTipoDeAtendimentoValidated] = useState(true);
+  const [mensagemValidated, setMensagemValidated] = useState(true);
 
-    function validateAppointment() {
-      let validated = 0;
-      const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (regexEmail.test(String(email).toLowerCase())) {
-        validated += 1;
-        setEmailValidated(true);
-      } else {
-        setEmailValidated(false);
-      }
-      if (nome.length > 3) {
-        validated += 1;
-        setNomeValidated(true);
-      } else {
-        setNomeValidated(false);
-      }
-      if (telefone.replace(/\D/g, "").length === 11) {
-        validated += 1;
-        setTelefoneValidated(true);
-      } else {
-        setTelefoneValidated(false);
-      }
-      if (idade != "") {
-        validated += 1;
-        setIdadeValidated(true);
-      } else {
-        setIdadeValidated(false);
-      }
-      if (tipoDeAtendimento != "") {
-        validated += 1;
-        setTipoDeAtendimentoValidated(true);
-      } else {
-        setTipoDeAtendimentoValidated(false);
-      }
-      setValidated(validated === 5);
+  function validateAppointment() {
+    let validatedFunc = 0;
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (regexEmail.test(String(email).toLowerCase())) {
+      validatedFunc += 1;
+      setEmailValidated(true);
+      console.log('email valido')
+    } else {
+      setEmailValidated(false);
     }
+    if (nome.length > 3) {
+      validatedFunc += 1;
+      setNomeValidated(true);
+      console.log('nome valido')
+    } else {
+      setNomeValidated(false);
+    }
+    if (mensagem.length > 3) {
+      validatedFunc += 1;
+      setMensagemValidated(true);
+      console.log('mensagem valida')
+    } else {
+      setMensagemValidated(false);
+    }
+    if (telefone.replace(/\D/g, "").length === 11) {
+      validatedFunc += 1;
+      setTelefoneValidated(true);
+      console.log('telefone valido')
+    } else {
+      setTelefoneValidated(false);
+    }
+    if (idade != "") {
+      validatedFunc += 1;
+      setIdadeValidated(true);
+      console.log('idade valida')
+    } else {
+      setIdadeValidated(false);
+    }
+    if (tipoDeAtendimento != "") {
+      validatedFunc += 1;
+      setTipoDeAtendimentoValidated(true);
+      console.log('tipo de atendimento valido' + tipoDeAtendimento)
+    } else {
+      setTipoDeAtendimentoValidated(false);
+    }
+    setValidated(validatedFunc === 6);
+    console.log(validated)
+    validatedFunc = 0;
+    if (validated) {
+      axios.defaults.headers.post['Content-Type'] = 'application/json';
+      axios.post('https://formsubmit.co/ajax/gui.peg@hotmail.com', {
+        nome: nome,
+        telefone: telefone,
+        email: email,
+        idade: idade,
+        profissional: profissional,
+        tipoDeAtendimento: tipoDeAtendimento,
+        assunto: assunto,
+        mensagem: mensagem
+      })
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+      window.location.replace('http://localhost:5173/obrigado.html'); /* ADICIONAR O ENDEREÇO NO LOCAL DE LOCALHOST:5173 PARA QUE ELE REDIRECIONE */
+    }
+  }
 
-    
+
 
 
 
   return (
-     <section className="contact_section" id="Contact">
-       <div className="contact_container">
-       <div className="greate_services_section_heading"><h1>Agende Sua Consulta</h1></div>
-       <div className="contact_section_heading">
+    <section className="contact_section" id="Contact">
+      <div className="contact_container">
+        <div className="greate_services_section_heading"><h1>Agende Sua Consulta</h1></div>
+        <div className="contact_section_heading">
           <p>Atendimentos de 9:00 às 22:00. <a href="https://calendar.google.com/calendar/u/0?cid=cGVkbWFjaGFkbzI3QGdtYWlsLmNvbQ" target="blank" class="hoverzin">Horários DISPONÍVEIS.</a></p>
-    </div> 
-    
-   
-    <form {...validated ? `action="https://formsubmit.co/gui.peg@hotmail.com" method="POST"` : ""}>
-            <div className="data_form_fields">
-              <label htmlFor="text"></label>
-              <input type="text" onChange={(event) => { setNome(event.target.value) }} name="Nome" id="text" required placeholder="Nome" />
-              <label htmlFor="idade"></label>
-              <input type="number" onChange={(event) => { setIdade(event.target.value) }} name="idade" id="idade" required placeholder="Idade" />
-              <label htmlFor="email"></label>
-              <input type="email" onChange={(event) => { setEmail(event.target.value) }} name="email" id="email" required placeholder="Email" />
-              <label htmlFor="profissional"></label>
-              <select onChange={(event) => { setProfissional(event.target.value) }} id="profissional" name="profissional">
-                <option value="livreEscolha">Escolha Por Mim</option>
+        </div>
+        <form>
+          <div className="data_form_fields">
+            <label htmlFor="text"></label>
+            <input type="text" className={nomeValidated ? '' : 'deniedForm'} onChange={(event) => { setNome(event.target.value) }} name="Nome" id="text" required placeholder="Nome" />
+            <label htmlFor="idade"></label>
+            <input type="number" className={idadeValidated ? '' : 'deniedForm'} onChange={(event) => { setIdade(event.target.value) }} name="idade" id="idade" required placeholder="Idade" />
+            <label htmlFor="email"></label>
+            <input type="email" className={emailValidated ? '' : 'deniedForm'} onChange={(event) => { setEmail(event.target.value) }} name="email" id="email" required placeholder="Email" />
+            <label htmlFor="profissional"></label>
+            <select onChange={(event) => { setProfissional(event.target.value) }} id="profissional" name="profissional">
+              <option value="livreEscolha">Escolha Por Mim</option>
               <option value="Profissinal1">Profissional1</option>
               <option value="Profissional2">Profissional2</option>
               <option value="Profissional3">Profissional3</option>
-              
-              </select>
-            </div>
-              <div className="data_form_fields">
-             <label htmlFor="number"></label>   
-             <input type="number" required onChange={(event) => { setTelefone(event.target.value) }} name="number" id="number" placeholder="Telefone" />
-             <label htmlFor="text"></label>
-             <input type="text" name="assunto" onChange={(event) => { setAssunto(event.target.value) }} id="assunto" required placeholder="Assunto" />
-              <label htmlFor="tipo-de-atendimento"></label>
-              <select id="tipo-de-atendimento" onChange={(event) => { setTipoDeAtendimento(event.target.value) }} name="tipo-de-atendimento">
+            </select>
+          </div>
+          <div className="data_form_fields">
+            <label htmlFor="number"></label>
+            <input type="number" className={telefoneValidated ? '' : 'deniedForm'} required onChange={(event) => { setTelefone(event.target.value) }} name="number" id="number" placeholder="Telefone" />
+            <label htmlFor="text"></label>
+            <input type="text" name="assunto" onChange={(event) => { setAssunto(event.target.value) }} id="assunto" required placeholder="Assunto" />
+            <label htmlFor="tipo-de-atendimento"></label>
+            <select id="tipo-de-atendimento" onChange={(event) => { setTipoDeAtendimento(event.target.value) }} name="tipo-de-atendimento">
               <option value="online">Consulta Online</option>
               <option value="presencial">Consulta Presencial</option>
-              </select>
-                <input type="hidden" name="_next" value="https://tp4-projetobloco.pedrohenriq1389.repl.co/" />
-           </div>
-              <div className="message_form_fields">
-                 <textarea required name="mensagem" onChange={(event) => { setMensagem(event.target.value) }} placeholder="Escreva aqui sua mensagem, incluindo Data e Hora que deseja marcar sua consulta. Em seguida, enviaremos uma mensagem no whatsapp para confirmar."></textarea>
-              </div>
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23039BE5&ctz=America%2FSao_Paulo&showTitle=1&showNav=1&mode=WEEK&showPrint=0&showTabs=0&showCalendars=1&src=cGVkbWFjaGFkbzI3QGdtYWlsLmNvbQ&color=%23039BE5" style={{borderWidth:0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
-          
-          <div className="form_submit_btn">
-              <button onClick={(e) => {e.preventDefault; validateAppointment()}} >ENVIAR</button>
+            </select>
+            <input type="hidden" name="_next" value="https://tp4-projetobloco.pedrohenriq1389.repl.co/" />
           </div>
-          </form>
-  </div>
-</section>
+          <div className="message_form_fields">
+            <textarea required className={mensagemValidated ? '' : 'deniedForm'} name="mensagem" onChange={(event) => { setMensagem(event.target.value) }} placeholder="Escreva aqui sua mensagem, incluindo Data e Hora que deseja marcar sua consulta. Em seguida, enviaremos uma mensagem no whatsapp para confirmar."></textarea>
+          </div>
+          <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23039BE5&ctz=America%2FSao_Paulo&showTitle=1&showNav=1&mode=WEEK&showPrint=0&showTabs=0&showCalendars=1&src=cGVkbWFjaGFkbzI3QGdtYWlsLmNvbQ&color=%23039BE5" style={{ borderWidth: 0 }} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+        </form>
+        <div className="form_submit_btn">
+          <button onClick={() => { console.log('clicked'); validateAppointment() }} >ENVIAR</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
