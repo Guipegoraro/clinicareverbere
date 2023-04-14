5dd2bfaacf2353639aaaf16ebf66e08e49aee18a
Guipegoraro <gui.peg@hotmail.com>
2023-04-14 17:15:23 -0300

    asdasd

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,23 @@
 import './Contact.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 function Contact() {
   const [nome, setNome] = useState('');
   const [idade, setIdade] = useState('');
   const [email, setEmail] = useState('');
-  const [profissional, setProfissional] = useState('livreEscolha');
+  const [profissional, setProfissional] = useState('livre Escolha');
   const [telefone, setTelefone] = useState('');
   const [assunto, setAssunto] = useState('');
   const [tipoDeAtendimento, setTipoDeAtendimento] = useState('');
   const [mensagem, setMensagem] = useState('');
-  const [validated, setValidated] = useState('');
   const [nomeValidated, setNomeValidated] = useState(true);
   const [idadeValidated, setIdadeValidated] = useState(true);
   const [emailValidated, setEmailValidated] = useState(true);
   const [telefoneValidated, setTelefoneValidated] = useState(true);
   const [tipoDeAtendimentoValidated, setTipoDeAtendimentoValidated] = useState(true);
   const [mensagemValidated, setMensagemValidated] = useState(true);
+  const [formsubmited, setFormSubmited] = useState(false);
 
   function validateAppointment() {
     let validatedFunc = 0;
@@ -33,7 +34,7 @@ function Contact() {
     } else {
       setNomeValidated(false);
     }
-    if (mensagem.length > 3) {
+    if (mensagem.length > 0) {
       validatedFunc += 1;
       setMensagemValidated(true);
     } else {
@@ -45,23 +46,20 @@ function Contact() {
     } else {
       setTelefoneValidated(false);
     }
-    if (idade != "") {
+    if (idade !== "") {
       validatedFunc += 1;
       setIdadeValidated(true);
-      console.log('idade valida')
     } else {
       setIdadeValidated(false);
     }
-    if (tipoDeAtendimento != "") {
+    if (tipoDeAtendimento === "online" || tipoDeAtendimento === "presencial") {
       validatedFunc += 1;
       setTipoDeAtendimentoValidated(true);
     } else {
       setTipoDeAtendimentoValidated(false);
     }
-    setValidated(validatedFunc === 6);
-    console.log(validated)
-    validatedFunc = 0;
-    if (validated) {
+    if (validatedFunc === 6) {
+      setFormSubmited(true);
       axios.defaults.headers.post['Content-Type'] = 'application/json';
       axios.post('https://formsubmit.co/ajax/gui.peg@hotmail.com', {
         nome: nome,
@@ -73,15 +71,14 @@ function Contact() {
         assunto: assunto,
         mensagem: mensagem
       })
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-      window.location.replace('http://localhost:5173/obrigado.html'); /* ADICIONAR O ENDEREÇO NO LOCAL DE LOCALHOST:5173 PARA QUE ELE REDIRECIONE */
+      .then(response => {
+        console.log(response);
+        window.location.replace('http://localhost:5173//obrigado.html');
+      })
+      .catch(error => console.log(error));
+      console.log('formulario enviado')
     }
   }
-
-
-
-
 
   return (
     <section className="contact_section" id="Contact">
@@ -90,7 +87,7 @@ function Contact() {
         <div className="contact_section_heading">
           <p>Atendimentos de 9:00 às 22:00. <a href="https://calendar.google.com/calendar/u/0?cid=cGVkbWFjaGFkbzI3QGdtYWlsLmNvbQ" target="blank" class="hoverzin">Horários DISPONÍVEIS.</a></p>
         </div>
-        <form>
+        <form onSubmit={(event) => {event.preventDefault();}}>
           <div className="data_form_fields">
             <label htmlFor="text"></label>
             <input type="text" className={nomeValidated ? '' : 'deniedForm'} onChange={(event) => { setNome(event.target.value) }} name="Nome" id="text" required placeholder="Nome" />
@@ -112,7 +109,8 @@ function Contact() {
             <label htmlFor="text"></label>
             <input type="text" name="assunto" onChange={(event) => { setAssunto(event.target.value) }} id="assunto" required placeholder="Assunto" />
             <label htmlFor="tipo-de-atendimento"></label>
-            <select id="tipo-de-atendimento" onChange={(event) => { setTipoDeAtendimento(event.target.value) }} name="tipo-de-atendimento">
+            <select id="tipo-de-atendimento"  className={tipoDeAtendimentoValidated ? '' : 'deniedForm'} onChange={(event) => { setTipoDeAtendimento(event.target.value) }} name="tipo-de-atendimento">
+              <option value="selecione">Selecione uma opção</option>
               <option value="online">Consulta Online</option>
               <option value="presencial">Consulta Presencial</option>
             </select>
@@ -122,10 +120,10 @@ function Contact() {
             <textarea required className={mensagemValidated ? '' : 'deniedForm'} name="mensagem" onChange={(event) => { setMensagem(event.target.value) }} placeholder="Escreva aqui sua mensagem, incluindo Data e Hora que deseja marcar sua consulta. Em seguida, enviaremos uma mensagem no whatsapp para confirmar."></textarea>
           </div>
           <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23039BE5&ctz=America%2FSao_Paulo&showTitle=1&showNav=1&mode=WEEK&showPrint=0&showTabs=0&showCalendars=1&src=cGVkbWFjaGFkbzI3QGdtYWlsLmNvbQ&color=%23039BE5" style={{ borderWidth: 0 }} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
-        </form>
         <div className="form_submit_btn">
-          <button onClick={() => { console.log('clicked'); validateAppointment() }} >ENVIAR</button>
+          {formsubmited ? <p>Formulário enviado!</p> : <button onClick={() => {validateAppointment() }} >ENVIAR</button>}
         </div>
+        </form>
       </div>
     </section>
   )
