b6b10fa66a037fe0d49b4948037a5d04bb8461ea
Guipegoraro <gui.peg@hotmail.com>
2023-04-14 14:19:51 -0300

    atualização readme

--- README.md
+++ README.md
@@ -39,6 +39,46 @@ missão
 valores
 texto sobre cada funcionario
 
+# texto sobre funcionarios
+Gizeli Cunha
+Psicóloga
+CRP 12/13826
+– Graduada em psicologia no ano de 2014 pela Faculdade CESUSC – Complexo de ensino superior de Santa Catarina.
+– Pós graduanda em Gestão da aprendizagem e educação cognitiva pela Faculdade Unicesumar.
+– Atendimentos pela abordagem da terapia cognitivo comportamental.
+– Experiência em atendimentos clínicos voltado para a criança a partir de 7 anos, nas demandas relacionadas a dificuldade de adaptação escolar e suas relações em ambientes sociais e familiares.
+ – Atendimento a adolescentes e adultos que buscam acolhimento e tratamento psicológico para Transtornos de Humor (depressão e bipolaridade), transtornos de ansiedade generalizada (TAG), Fobias (específicas, social, agorafobia), transtorno de pânico, burnout, estresse pós-traumático, transtornos mentais e comportamentais relacionados ao uso de abusivo de drogas, luto, Transtorno Obsessivo-compulsivo (TOC), entre outras demandas psicológicas.
+– Avaliação Psicológica para cirurgia de vasectomia.
+Atendimentos para crianças a partir de 7 anos, adolescentes, adultos e idosos.
+Atendimento com criança somente presencial e os demais podem ser realizados on-line.
+
+KELI ADRIANA M. GODOI
+
+Psicóloga
+
+CRP 12/12834
+
+– Graduada pela Faculdade CESUSC.
+– Atuante na área clínica há seis anos, sob o foco da Psicologia Relacional Sistêmica.
+– Atendimentos voltados para adultos que buscam tratamento para transtornos depressivos e relacionados a traumas e estressores.
+– Avaliação Psicológica para cirurgia bariátrica e vasectomia.
+
+Atende adultos.
+
+RONILDA ETTER BRUSCH
+
+Psicóloga
+
+CRP 07/21926
+
+– Graduada pela Universidade Feevale-RS.
+– Pós-graduanda em Psicologia Positiva e Coaching pela Unileyal.
+– Pós graduanda em Psicologia do Trabalho e das organizações pela Estácio.
+– Formação voltada à Psicoterapia e controle do Estresse, Transtornos de Ansiedade e Síndrome do Pânico.
+– Atendimentos voltados a pacientes que sofrem de Transtorno Obsessivo-Compulsivo (TOC), Transtornos de Humor (depressão e bipolaridade), Fobias (específicas, social, agorafobia) e Transtornos Alimentares.
+
+Atende crianças, adolescentes e adultos.
+
 
 # TODO
 adicionar "letras miudas" sobre LGPD nos formulários
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,61 @@
 import './Contact.css';
-
+import React, { useState } from 'react';
 function Contact () {
+    const [nome, setNome] = useState('');
+    const [idade, setIdade] = useState('');
+    const [email, setEmail] = useState('');
+    const [profissional, setProfissional] = useState('livreEscolha');
+    const [telefone, setTelefone] = useState('');
+    const [assunto, setAssunto] = useState('');
+    const [tipoDeAtendimento, setTipoDeAtendimento] = useState('');
+    const [mensagem, setMensagem] = useState('');
+    const [validated, setValidated] = useState('');
+    const [nomeValidated, setNomeValidated] = useState('');
+    const [idadeValidated, setIdadeValidated] = useState('');
+    const [emailValidated, setEmailValidated] = useState('');
+    const [telefoneValidated, setTelefoneValidated] = useState('');
+    const [tipoDeAtendimentoValidated, setTipoDeAtendimentoValidated] = useState('');
+
+    function validateAppointment() {
+      let validated = 0;
+      const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (regexEmail.test(String(email).toLowerCase())) {
+        validated += 1;
+        setEmailValidated(true);
+      } else {
+        setEmailValidated(false);
+      }
+      if (nome.length > 3) {
+        validated += 1;
+        setNomeValidated(true);
+      } else {
+        setNomeValidated(false);
+      }
+      if (telefone.replace(/\D/g, "").length === 11) {
+        validated += 1;
+        setTelefoneValidated(true);
+      } else {
+        setTelefoneValidated(false);
+      }
+      if (idade != "") {
+        validated += 1;
+        setIdadeValidated(true);
+      } else {
+        setIdadeValidated(false);
+      }
+      if (tipoDeAtendimento != "") {
+        validated += 1;
+        setTipoDeAtendimentoValidated(true);
+      } else {
+        setTipoDeAtendimentoValidated(false);
+      }
+      setValidated(validated === 5);
+    }
+
+    
+
+
+
   return (
      <section className="contact_section" id="Contact">
        <div className="contact_container">
@@ -8,18 +63,19 @@ function Contact () {
        <div className="contact_section_heading">
           <p>Atendimentos de 9:00 às 22:00. <a href="https://calendar.google.com/calendar/u/0?cid=cGVkbWFjaGFkbzI3QGdtYWlsLmNvbQ" target="blank" class="hoverzin">Horários DISPONÍVEIS.</a></p>
     </div> 
-
-          <form action="https://formsubmit.co/gui.peg@hotmail.com" method="POST">
+    
+   
+    <form {...validated ? `action="https://formsubmit.co/gui.peg@hotmail.com" method="POST"` : ""}>
             <div className="data_form_fields">
               <label htmlFor="text"></label>
-              <input type="text" name="Nome" id="text" required placeholder="Nome" />
+              <input type="text" onChange={(event) => { setNome(event.target.value) }} name="Nome" id="text" required placeholder="Nome" />
               <label htmlFor="idade"></label>
-              <input type="number" name="idade" id="idade" required placeholder="Idade" />
+              <input type="number" onChange={(event) => { setIdade(event.target.value) }} name="idade" id="idade" required placeholder="Idade" />
               <label htmlFor="email"></label>
-              <input type="email" name="email" id="email" required placeholder="Email" />
+              <input type="email" onChange={(event) => { setEmail(event.target.value) }} name="email" id="email" required placeholder="Email" />
               <label htmlFor="profissional"></label>
-              <select id="profissional" name="profissional">
-                <option value="random">Escolha Por Mim</option>
+              <select onChange={(event) => { setProfissional(event.target.value) }} id="profissional" name="profissional">
+                <option value="livreEscolha">Escolha Por Mim</option>
               <option value="Profissinal1">Profissional1</option>
               <option value="Profissional2">Profissional2</option>
               <option value="Profissional3">Profissional3</option>
@@ -28,23 +84,23 @@ function Contact () {
             </div>
               <div className="data_form_fields">
              <label htmlFor="number"></label>   
-             <input type="number" name="number" id="number" placeholder="Telefone" />
+             <input type="number" required onChange={(event) => { setTelefone(event.target.value) }} name="number" id="number" placeholder="Telefone" />
              <label htmlFor="text"></label>
-             <input type="text" name="assunto" id="assunto" required placeholder="Assunto" />
+             <input type="text" name="assunto" onChange={(event) => { setAssunto(event.target.value) }} id="assunto" required placeholder="Assunto" />
               <label htmlFor="tipo-de-atendimento"></label>
-              <select id="tipo-de-atendimento" name="tipo-de-atendimento">
+              <select id="tipo-de-atendimento" onChange={(event) => { setTipoDeAtendimento(event.target.value) }} name="tipo-de-atendimento">
               <option value="online">Consulta Online</option>
               <option value="presencial">Consulta Presencial</option>
               </select>
                 <input type="hidden" name="_next" value="https://tp4-projetobloco.pedrohenriq1389.repl.co/" />
            </div>
               <div className="message_form_fields">
-                 <textarea name="mensagem" placeholder="Escreva aqui sua mensagem, incluindo Data e Hora que deseja marcar sua consulta. Em seguida, enviaremos uma mensagem no whatsapp para confirmar."></textarea>
+                 <textarea required name="mensagem" onChange={(event) => { setMensagem(event.target.value) }} placeholder="Escreva aqui sua mensagem, incluindo Data e Hora que deseja marcar sua consulta. Em seguida, enviaremos uma mensagem no whatsapp para confirmar."></textarea>
               </div>
             <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23039BE5&ctz=America%2FSao_Paulo&showTitle=1&showNav=1&mode=WEEK&showPrint=0&showTabs=0&showCalendars=1&src=cGVkbWFjaGFkbzI3QGdtYWlsLmNvbQ&color=%23039BE5" style={{borderWidth:0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
           
           <div className="form_submit_btn">
-              <button>ENVIAR</button>
+              <button onClick={(e) => {e.preventDefault; validateAppointment()}} >ENVIAR</button>
           </div>
           </form>
   </div>
